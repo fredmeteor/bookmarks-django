c5940b2dad33d83dea8bcd7b996c8247cc30390c
fredmeteor <fredpeter09899@gmail.com>
2024-09-02 10:51:25 +0300

    Added registration,dashboard templates and static cssc files

--- account/views.py
+++ account/views.py
@@ -1,7 +1,10 @@
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 from .forms import LoginForm
+
+
 def user_login(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
@@ -23,3 +26,12 @@ def user_login(request):
     else:
         form = LoginForm()
     return render(request, 'account/login.html', {'form': form})
+
+
+@login_required
+def dashboard(request):
+    return render(
+        request,
+        'account/dashboard.html',
+        {'section': 'dashboard'}
+    )
